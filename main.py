609144a55193a9ea2b2ebd10316075db4f99e8f8
Romuz <romuzus08@gmail.com>
2021-01-16 15:18:08 -0800

    Refactor

--- main.py
+++ main.py
@@ -1,115 +1,118 @@
 import random
 
-def validateMove(playerMove, currentBoard):
-    if len(playerMove) != 2:
+currBoard = [
+    2, 2, 2,
+    0, 0, 0,
+    1, 1, 1
+]
+
+AIprevMoves = []
+
+def validateMove(playerMove):
+    if playerMove not in possiblePlayerMoves:
         return False
-    if currentBoard[playerMove[0] - 1] != 1:
+    if currBoard[playerMove[0] - 1] != 1:
         return False
-    if playerMove[0] <= playerMove[1] or playerMove[0] > playerMove[1] + 4:
-        return False
-    if playerMove[1] == playerMove[0] - 3 and currentBoard[playerMove[1] - 1] != 0:
+    if playerMove[1] == playerMove[0] - 3 and currBoard[playerMove[1] - 1] != 0:
         return False
     if playerMove[0] in [7, 4]:
-        if playerMove[1] == playerMove[0] - 2 and currentBoard[playerMove[1] - 1] != 2:
+        if playerMove[1] == playerMove[0] - 2 and currBoard[playerMove[1] - 1] != 2:
             return False
         elif playerMove[1] in [6, 3]:
             return False
     elif playerMove[0] in [8, 5]:
-        if playerMove[1] == playerMove[0] - 4 and currentBoard[playerMove[1] - 1] != 2:
+        if playerMove[1] == playerMove[0] - 4 and currBoard[playerMove[1] - 1] != 2:
             return False
-        elif playerMove[1] == playerMove[0] - 2 and currentBoard[playerMove[1] - 1] != 2:
+        elif playerMove[1] == playerMove[0] - 2 and currBoard[playerMove[1] - 1] != 2:
             return False
         elif playerMove[1] == playerMove[0] - 1:
             return False
     else: # playerMove[0] in [9, 6]
-        if playerMove[1] == playerMove[0] - 4 and currentBoard[playerMove[1] - 1] != 2:
+        if playerMove[1] == playerMove[0] - 4 and currBoard[playerMove[1] - 1] != 2:
             return False
         elif playerMove[1] >= playerMove[0] - 2:
             return False
     return True
 
-def printCurrentBoard(board):
-    print(board[0], board[1], board[2])
-    print(board[3], board[4], board[5])
-    print(board[6], board[7], board[8])
+def printCurrBoard():
+    print(currBoard[0], currBoard[1], currBoard[2])
+    print(currBoard[3], currBoard[4], currBoard[5])
+    print(currBoard[6], currBoard[7], currBoard[8])
+    print('\n')
 
-def main():
+def initMatch():
     print('This is what each number move means in a board')
     print('1 2 3\n4 5 6\n7 8 9\n')
     print('This is what the board looks like right now:')
-    print('2 2 2\n0 0 0\n1 1 1')
-    currentBoard = [
+    global currBoard
+    currBoard = [
         2, 2, 2,
         0, 0, 0,
         1, 1, 1
     ]
+    printCurrBoard()
+    global AIprevMoves
     AIprevMoves = []
+
+def playerWon():
+    print('\nTHE PLAYER WON!\n')
+    print('AI gets smarter.\n')
+    AImoves[AIprevMoves[-1].get('id') - 1]['moves'].remove(AIprevMoves[-1].get('move'))
+    if not AImoves[AIprevMoves[-1].get('id') - 1]['moves']:
+        AImoves[AIprevMoves[-2].get('id') - 1]['moves'].remove(AIprevMoves[-2].get('move'))
+    initMatch()
+
+def AIwon():
+    print('\nTHE AI WON!\n')
+    initMatch()
+
+def main():
+    initMatch()
     while True:
-        try:
-            playerMove = [int(i) for i in str(input('enter:'))]
-            if not validateMove(playerMove, currentBoard):
-                print('Invalid input')
-                continue
-            currentBoard[playerMove[0] - 1] = 0
-            currentBoard[playerMove[1] - 1] = 1
-            print('\nYour Move:\n')
-            printCurrentBoard(currentBoard)
-            print('\nAI Move:\n')
-            for move in AImoves:
-                if move.get('board') == currentBoard:
-                    AImove = move
-            if not AImove.get('moves'):
-                print('\nTHE PLAYER WON!\n')
-                print('AI gets smarter.')
-                currentBoard = [
-                    2, 2, 2,
-                    0, 0, 0,
-                    1, 1, 1
-                ]
-                AImoves[AIprevMoves[-1].get('id') - 1]['moves'].remove(AIprevMoves[-1].get('move'))
-                if not AImoves[AIprevMoves[-1].get('id') - 1]['moves']:
-                    AImoves[AIprevMoves[-2].get('id') - 1]['moves'].remove(AIprevMoves[-2].get('move'))
-                AIprevMoves = []
-                print('This is what the board looks like right now:')
-                print('2 2 2\n0 0 0\n1 1 1')
-                continue
-            move = [int(i) for i in str(random.choice(AImove.get('moves')))]
-            currentBoard[move[0] - 1] = 0
-            currentBoard[move[1] - 1] = 2
-            AIprevMoves.append({
-                'id': AImove.get('id'),
-                'move': int(''.join([str(n) for n in move]))
-            })
-            printCurrentBoard(currentBoard)
-            if 2 in currentBoard[6:] or 1 not in currentBoard:
-                print('\nTHE AI WON!\n')
-                currentBoard = [
-                    2, 2, 2,
-                    0, 0, 0,
-                    1, 1, 1
-                ]
-                AIprevMoves = []
-                print('This is what the board looks like right now:')
-                print('2 2 2\n0 0 0\n1 1 1')
-                continue
-            for n in possiblePlayerMoves:
-                if validateMove(n, currentBoard):
-                    break
-            else:
-                print('\nTHE AI WON!\n')
-                currentBoard = [
-                    2, 2, 2,
-                    0, 0, 0,
-                    1, 1, 1
-                ]
-                AIprevMoves = []
-                print('This is what the board looks like right now:')
-                print('2 2 2\n0 0 0\n1 1 1')
-                continue
-        except Exception as e:
-            print(e)
+        playerMove = [int(i) for i in str(input('Enter Your Move:'))]
+
+        if not validateMove(playerMove):
+            print('Invalid input')
+            continue
+
+        currBoard[playerMove[0] - 1], currBoard[playerMove[1] - 1] = 0, 1
+
+        print('\nYour Move:\n')
+        printCurrBoard()
+
+        for move in AImoves:
+            if move.get('board') == currBoard:
+                AImove = move
+
+        if not AImove.get('moves'):
+            playerWon()
+            continue
+
+        move = [int(i) for i in str(random.choice(AImove.get('moves')))]
+
+        currBoard[move[0] - 1], currBoard[move[1] - 1] = 0, 2
+
+        AIprevMoves.append({
+            'id': AImove.get('id'),
+            'move': int(''.join([str(n) for n in move]))
+        })
+
+        print('\nAI Move:\n')
+        printCurrBoard()
+
+        if 2 in currBoard[6:] or 1 not in currBoard:
+            AIwon()
+            continue
+
+        for n in possiblePlayerMoves:
+            if validateMove(n):
+                break
+        else:
+            AIwon()
+            continue
 
 possiblePlayerMoves = [[7, 4], [7, 5], [8, 4], [8, 5], [8, 6], [9, 5], [9,  6], [4, 1], [4, 2], [5, 1], [5, 2], [5, 3], [6, 2], [6, 3]]
+
 AImoves = [
     # AI's first moves:
     {'id': 1, 'moves': [24, 25, 36], 'board': [
